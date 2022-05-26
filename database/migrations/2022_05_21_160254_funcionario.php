48b382d32fbc3c8945efe6faa8b14e407fe475d6
RaulGoncalves21x <raulmago100@gmail.com>
2022-05-26 19:08:45 +0100

    Correcao chaves estrangeiras migrations

---
+++ database/migrations/2022_05_21_160254_funcionario.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionario', function (Blueprint $table) {
+            $table->integer('nif')->unsigned()->primary();
+            $table->string('nome')->notNull();
+            $table->integer('idade')->notNull();
+            $table->string('email')->unique()->notNull();
+            $table->integer('telemovel')->unique()->notNull();
+            $table->string('especializacao')->notNull();
+            $table->int('departamento_id')->notNull();
+            $table->foreign('departamento_id')->references('id')->on('departamento')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('funcionario');
+    }
+};
