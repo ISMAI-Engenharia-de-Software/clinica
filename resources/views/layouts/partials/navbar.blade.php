dd72b2a0db9eff4b9e5f08f3552c60edf8b904d3
Cacheira14 <a038692@ismai.pt>
2022-06-01 22:30:20 +0100

    Atualizaçoes navbar
    Navbar atualizada a fim de parte da lista "Testes" redirecionar utilizador, através de uma rota, para o index dos testes;
    Navbar aplicada à página de raiz (welcome.blade.php)

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -22,7 +22,7 @@
                     <a class="nav-link" href="#">Relatórios</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Testes</a>
+                    <a class="nav-link" href="{{ route('teste.index') }}">Testes</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Funcionários</a>
