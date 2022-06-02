73a3ba35fdfc8f4a8045098926aa52627b276c38
AndreAlves17 <a039045@ismai.pt>
2022-06-02 16:12:30 +0100

    update funcionario controller

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Funcionario;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\DB;
 
 class FuncionarioController extends Controller
 {
@@ -19,5 +22,10 @@ class FuncionarioController extends Controller
     public function mostrar_reg(Funcionario $funcionario)
     {
         return view('funcionario.mostrar_reg', ['funcionario' => $funcionario]);
+    }
+    public function criar()
+    {
+        Funcionario::create($this->validateFuncionario());
+        return (redirect(route('funcionario.index'))->with('success', 'Funcionario criado com sucesso'));
     }
 }
