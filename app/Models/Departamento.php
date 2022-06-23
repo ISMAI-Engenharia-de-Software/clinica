6a72abfa8338fc6763ae90ffcca700b0c3fee3a2
AlexandrePinheiroo <A039119@ismai.pt>
2022-06-23 12:14:33 +0100

    Update Departamento.php

---
+++ app/Models/Departamento.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Departamento extends Model
+{
+    use HasFactory;
+
+    public $table = 'departamento';
+
+    protected $fillable = [
+        'Nome',
+        'AreaDepartamento',
+        'NumTrabalhadores',
+        'Responsavel'];
+}
